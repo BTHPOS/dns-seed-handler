87d03da025552a45096ee71a17b5a448a49af5dc
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-02-08 15:09:15 -0500

    Updated DNS to be dynamic

--- server.js
+++ server.js
@@ -1,50 +1,61 @@
 var dnsd = require('dnsd');
-var PORT = 53;
+var got = require("got");
+var PORT = 5333;
 var INTERFACEIP = '0.0.0.0'
+var limit = 20; // max number of hosts
+var addresses = [];
+
+var fetchAddresses = function() {
+  got("http://explorer.bithereum.network/ext/connections?_=1549651810200")
+     .then(function(response) {
+            var data = {}
+            try {
+                data = JSON.parse(response.body);
+            } catch(e) {}
+
+            if (data.data) {
+                addresses = data.data.splice(0,limit).map(function(entry) {
+                    return entry.address;
+                });
+            }
+      });
+};
 
 var handle = function(req, res) {
    var question = req.question[0];
    var hostname = question.name || "";
    var length = hostname.length;
-   var ttl = Math.floor(Math.random() * 3600);
+   var ttl = Math.floor(Math.random() * 60);
    console.log(question);
-   
+
    if (question.type == "A") {
       // US (Mainnet)
       if (hostname.indexOf("x9.us-dnsseed") === 0) {
-        res.answer.push({name:hostname, type:'A', data:"54.89.190.95", ttl:ttl});
-        res.answer.push({name:hostname, type:'A', data:"54.90.61.152", ttl:ttl});
-        res.answer.push({name:hostname, type:'A', data:"3.83.48.239", ttl:ttl});
+            for (var address in addresses) {
+                res.answer.push({name:hostname, type:'A', data: addresses[address], ttl:ttl});
+            }
+            fetchAddresses();
       }
       // Europe (Mainnet)
       else if (hostname.indexOf("x9.eu-dnsseed") === 0) {
-        res.answer.push({name:hostname, type:'A', data:"", ttl:ttl});
-        res.answer.push({name:hostname, type:'A', data:"", ttl:ttl});
       }
       // Singapore (Mainnet)
       else if (hostname.indexOf("x9.sg-dnsseed") === 0) {
-        res.answer.push({name:hostname, type:'A', data:"", ttl:ttl});
-        res.answer.push({name:hostname, type:'A', data:"", ttl:ttl});
       }
       // US (Testnet)
       else if (hostname.indexOf("x9.us-testnet-dnsseed") === 0) {
-        res.answer.push({name:hostname, type:'A', data:"18.212.4.44", ttl:ttl});
-        res.answer.push({name:hostname, type:'A', data:"54.162.119.37", ttl:ttl});
       }
       // Europe (Testnet)
       else if (hostname.indexOf("x9.eu-testnet-dnsseed") === 0) {
-        res.answer.push({name:hostname, type:'A', data:"18.212.4.44", ttl:ttl});
-        res.answer.push({name:hostname, type:'A', data:"54.162.119.37", ttl:ttl});
       }
       // Singapore (Testnet)
       else if (hostname.indexOf("x9.sg-testnet-dnsseed") === 0) {
-        res.answer.push({name:hostname, type:'A', data:"18.212.4.44", ttl:ttl});
-        res.answer.push({name:hostname, type:'A', data:"54.162.119.37", ttl:ttl});
       }
    }
 
    res.end()
 };
 
+fetchAddresses();
 dnsd.createServer(handle).listen(PORT, INTERFACEIP);
 console.log('DNS Server Running');
